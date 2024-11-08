1013358c9c0844843c3e0374e9b7246b1b790d99
Jaejune <jjune0427@naver.com>
2024-11-08 22:05:25 +0900

    Unity Project 진행중

---
+++ Source/Slash/Private/Enemy/Mage/MageEnemy.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy/Mage/MageEnemy.h"
+
+AMageEnemy::AMageEnemy()
+{
+    
+}
---
+++ Source/Slash/Public/Enemy/Mage/MageEnemy.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/Enemy.h"
+#include "MageEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLASH_API AMageEnemy : public AEnemy
+{
+	GENERATED_BODY()
+public:
+	AMageEnemy();
+};
