81c09f5996944948eaa47b85c8a809c396eff0e9
Jaejune <jjune0427@naver.com>
2025-01-15 00:13:45 +0900

    warlord 추가

---
+++ Source/Slash/Public/Enemy/WarlordEnemy.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/Warrior/WarriorEnemy.h"
+#include "WarlordEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLASH_API AWarlordEnemy : public AWarriorEnemy
+{
+	GENERATED_BODY()
+public:
+	AWarlordEnemy();
+};
