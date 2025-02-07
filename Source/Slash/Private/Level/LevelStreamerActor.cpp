6f36c27af1c34b7fe07f9a44bc5412db6b76cb36
Jaejune <jjune0427@naver.com>
2025-02-07 20:58:12 +0900

    Level Streaming Distance 및 Delay 추가

--- Source/Slash/Private/Level/LevelStreamerActor.cpp
+++ Source/Slash/Private/Level/LevelStreamerActor.cpp
@@ -2,6 +2,7 @@
 #include "Level/LevelStreamerActor.h"
 #include "GameFramework/Character.h"
 #include "Components/BoxComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "Kismet/GameplayStatics.h"
 
 ALevelStreamerActor::ALevelStreamerActor()
@@ -11,19 +12,60 @@ ALevelStreamerActor::ALevelStreamerActor()
 
 	OverlapVolume->OnComponentBeginOverlap.AddUniqueDynamic(this, &ALevelStreamerActor::OverlapBegins);
 	OverlapVolume->OnComponentEndOverlap.AddUniqueDynamic(this, &ALevelStreamerActor::OverlapEnds);
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 }
 
 // Called when the game starts or when spawned
 void ALevelStreamerActor::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+
+    Player = UGameplayStatics::GetPlayerCharacter(this, 0);
+    if (!Player) return;
+
+    FVector PlayerLocation = Player->GetActorLocation();
+    OverlapVolumeLocation = OverlapVolume->GetComponentLocation();
+    float Distance = FVector::Dist(PlayerLocation, OverlapVolumeLocation);
+
+    if (Distance < UnLoadDistance && LevelToLoad != "")
+    {
+        Player->GetCharacterMovement()->SetMovementMode(MOVE_Flying);
+
+        FTimerHandle LoadTimer;
+        GetWorldTimerManager().SetTimer(LoadTimer, this, &ALevelStreamerActor::DelayedLoadLevel, 0.1f, false);
+    }
+}
+
+void ALevelStreamerActor::DelayedLoadLevel()
+{
+    FLatentActionInfo LatentInfo;
+    UGameplayStatics::LoadStreamLevel(this, LevelToLoad, true, true, LatentInfo);
+
+    Player->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
+}
+
+void ALevelStreamerActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if(bUseUnLoadDistance && bIsCheckingDistance){
+		FVector PlayerLocation = Player->GetActorLocation();
+		OverlapVolumeLocation = OverlapVolume->GetComponentLocation();
+		float Distance = FVector::Dist(PlayerLocation, OverlapVolumeLocation);
+
+		if(Distance > UnLoadDistance){
+			bIsCheckingDistance = false;
+			if(bUseUnLoadDelay){
+				GetWorldTimerManager().SetTimer(UnloadTimer, this, &ALevelStreamerActor::UnloadLevel, UnLoadDelay, false);
+			}else{
+				UnloadLevel();
+			}
+		}
+	}
 }
 
 void ALevelStreamerActor::OverlapBegins(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	ACharacter* Player = UGameplayStatics::GetPlayerCharacter(this, 0);
+	Player = UGameplayStatics::GetPlayerCharacter(this, 0);
 	ACharacter* OverlapedCharacter = Cast<ACharacter>(OtherActor);
 
 	if(Player == OverlapedCharacter && LevelToLoad != "")
@@ -35,12 +77,23 @@ void ALevelStreamerActor::OverlapBegins(UPrimitiveComponent* OverlappedComponent
 
 void ALevelStreamerActor::OverlapEnds(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	ACharacter* Player = UGameplayStatics::GetPlayerCharacter(this, 0);
+	Player = UGameplayStatics::GetPlayerCharacter(this, 0);
 	ACharacter* OverlapedCharacter = Cast<ACharacter>(OtherActor);
 
 	if (Player == OverlapedCharacter && LevelToLoad != "")
 	{
-		FLatentActionInfo LatentInfo;
-		UGameplayStatics::UnloadStreamLevel(this, LevelToLoad, LatentInfo,false);
+		if(bUseUnLoadDistance){
+			bIsCheckingDistance = true;
+		}else if(bUseUnLoadDelay){
+			GetWorldTimerManager().SetTimer(UnloadTimer, this, &ALevelStreamerActor::UnloadLevel, UnLoadDelay, false);
+		}else{
+			UnloadLevel();
+		}
 	}
+}
+
+void ALevelStreamerActor::UnloadLevel()
+{
+	FLatentActionInfo LatentInfo;
+	UGameplayStatics::UnloadStreamLevel(this, LevelToLoad, LatentInfo, false);
 }
--- Source/Slash/Public/Level/LevelStreamerActor.h
+++ Source/Slash/Public/Level/LevelStreamerActor.h
@@ -16,17 +16,37 @@ class SLASH_API ALevelStreamerActor : public AActor
 public:	
 	// Sets default values for this actor's properties
 	ALevelStreamerActor();
+	virtual void Tick(float DeltaTime) override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	void DelayedLoadLevel();
+	ACharacter* Player;
+	FVector OverlapVolumeLocation;
+
+	//UnLoad Delay
+	UPROPERTY(EditAnywhere)
+	bool bUseUnLoadDelay = false;
+	UPROPERTY(EditAnywhere)
+	float UnLoadDelay = 1.0f;
+	FTimerHandle UnloadTimer;
+
+	//UnLoad Distance
+	UPROPERTY(EditAnywhere)
+	bool bUseUnLoadDistance = false;
+	bool bIsCheckingDistance = false;
+	UPROPERTY(EditAnywhere)
+	float UnLoadDistance = 1000.0f;
+
 	UPROPERTY(EditAnywhere)
 	FName LevelToLoad;
 	UFUNCTION()
 	void OverlapBegins(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 	UFUNCTION()	
 	void OverlapEnds(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
-
+	UFUNCTION()
+	void UnloadLevel();
 
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
